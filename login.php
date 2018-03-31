e93f6865bc37314543cabde28333e81136d70c8f
Trey Helms III <jlhelms@ada7.cs.clemson.edu>
2018-03-31 11:49:24 -0400

    added navbar to login

--- login.php
+++ login.php
@@ -28,7 +28,9 @@ if(isset($_POST['submit'])) {
 ?>
 	<form method="post" action="<?php echo "login.php"; ?>">
 
-	<table width="100%">
+<!-----Previous HTML that is commented currently ---->
+
+	<!---<table width="100%">
 		<tr>
 			<td  width="20%">Username:</td>
 			<td width="80%"><input class="text"  type="text" name="username"><br /></td>
@@ -42,7 +44,61 @@ if(isset($_POST['submit'])) {
 			<td><input name="submit" type="submit" value="Login"><input name="reset" type="reset" value="Reset"><br /></td>
 		</tr>
 	</table>
-	</form>
+	</form>--->
+
+
+<!---------------- Navigation Bar --------->	
+
+ <div id="Navigation_Bar">
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+		
+  
+ 
+  <!-- Links that are located on the Navagation Bar -->
+  
+	<!-- Left Nav Bar Elements -->
+		
+  <ul class="navbar-nav navbar-left">
+	  <a class="navbar-brand" href="#">METUBE</a>
+    <li class="nav-item">
+      <a class="nav-link" href="#">IMAGE</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">VIDEO</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">AUDIO</a>
+    </li>
+   </ul>
+	    
+	
+	  <!-- Right Nav Bar Elements -->
+	<ul class="nav navbar-nav navbar-right">
+		
+		<!-- Search bar -->
+		<form class="navbar-form navbar-right" action="#" style="padding-right: 20px; width: 450px">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search" name="search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+				 <i class="fas fa-search"></i>
+       	 </button>
+        </div>
+      </div>
+    </form>
+	  
+		<li class="nav-item">
+		  <a class="nav-link" href="register.php"> Register</a>
+	  </li>
+	  <li class="nav-item"> 
+		  <a class="nav-link" href="login.php"> Login</a>
+		</li>
+ </ul>
+	
+</div>
+</nav>
+</div>
 
 <?php
   if(isset($login_error))
