770d77a0229d03781a3bdf12c19312fded445dd9
Trey Helms <thelms3rd@89.138.21.198.guest.wifi.dyn.clemson.edu>
2018-04-03 18:16:20 -0400

    config user updated

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $dbhost = "mysql1.cs.clemson.edu";
-$dbuser = "MTbDtbs_roe7@www1.cs.clemson.edu";
+$dbuser = "MTbDtbs_roe7";
 $dbpass = "MeTubeDatabase1!";
 $database = "MeTubeDatabase_q84v";
 ?>
