b4528379dd464b03c22e785b551d890dfa5acec9
Trey Helms <thelms3rd@155.6.23.172.generic.clemson.edu>
2018-04-03 15:22:18 -0400

    original index.php for connecting db

--- index.php
+++ index.php
@@ -1,15 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
+<body>
+<?php
+echo "<h1> Welcome to Metube! </h1>";
+?>
+
+<form action="login.php" method="post">
+	
+	<input type="submit" class="button"  VALUE = "Log in" >
+</form>
+
+<form action="register.php" method="post">
+	
+	<input type="submit" class="button"  VALUE = "Register" >
+</form>
+
+</body>
+</html>
+
+
+
+
  
-<head>
+<!----- MY CODE ------------> 
+<!DOCTYPE html>
+<!---   <html lang="en">
+ 
+<head>   
     <!-- Required meta tags -->
-    <meta charset="utf-8">
+  <!---  <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-	<!-- Font_Awesome -->
+	<!-- Font_Awesome 
 	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 	
     <title>METUBE</title>
@@ -17,19 +41,19 @@
 
 	
 	
-<!---------------------------------- BEGINNING OF THE BODY ------------------------------->
+<!---------------------------------- BEGINNING OF THE BODY -------------------------------
 <body>
 	
-<!--------------------------- Navigation Bar -------------------------------------------->	
+<!--------------------------- Navigation Bar --------------------------------------------
   <div id="Navigation_Bar">
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
 		
   
  
-  <!-- Links that are located on the Navagation Bar -->
+  <!-- Links that are located on the Navagation Bar --
   
-	<!-- Left Nav Bar Elements -->
+	<!-- Left Nav Bar Elements --
 		
   <ul class="navbar-nav navbar-left">
 	  <a class="navbar-brand" href="#">METUBE</a>
@@ -45,10 +69,10 @@
    </ul>
 	    
 	
-	  <!-- Right Nav Bar Elements -->
+	  <!-- Right Nav Bar Elements --
 	<ul class="nav navbar-nav navbar-right">
 		
-		<!-- Search bar -->
+		<!-- Search bar --
 		<form class="navbar-form navbar-right" action="#" style="padding-right: 20px; width: 450px">
       <div class="input-group">
         <input type="text" class="form-control" placeholder="Search" name="search">
@@ -72,33 +96,34 @@
 </nav>
 </div>
 
-<!----------------------------MIDDLE SECTION ----------------------->
+<!----------------------------MIDDLE SECTION -----------------------
 	
 <div id="Middle" class="container-fluid">
 
 
-<!---  Display the Most Viewed Media --->
+<!---  Display the Most Viewed Media ---
 <br>
 <h5> MOST VIEWED: </h5>	
 
 <hr>
 
-<!--- Display the New Uploads ---->
+<!--- Display the New Uploads ----
 <br>
 <h5>  NEW UPLOADS: </h5>
 	
 </div>
 	
 	
-<!---------------------------------------------------------------------->
+<!----------------------------------------------------------------------
 
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- Optional JavaScript --
+    <!-- jQuery first, then Popper.js, then Bootstrap JS --
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
+---->
