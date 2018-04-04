ad39ae9f3611d96f711fa5db14166380847177a9
mpalme2 <mpalme2@g.clemson.edu>
2018-04-04 13:36:29 -0400

    browse

--- browse.php
+++ browse.php
@@ -1,66 +1,59 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php
-	session_start();
-	include_once "function.php";
-?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Media browse</title>
-<link rel="stylesheet" type="text/css" href="css/default.css" />
-<script type="text/javascript" src="js/jquery-latest.pack.js"></script>
-<script type="text/javascript">
-function saveDownload(id)
-{
-	$.post("media_download_process.php",
-	{
-       id: id,
-	},
-	function(message) 
-    { }
- 	);
-} 
-</script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<!-- Font_Awesome -->
+	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
+    <title>METUBE</title>
 </head>
 
 <body>
 <p>Welcome <?php echo $_SESSION['username'];?></p>
 <a href='media_upload.php'  style="color:#FF9900;">Upload File</a>
 <div id='upload_result'>
-<?php 
+<?php
 	if(isset($_REQUEST['result']) && $_REQUEST['result']!=0)
-	{		
+	{
 		echo upload_error($_REQUEST['result']);
 	}
 ?>
 </div>
 <br/><br/>
 <?php
-
-	$query = "SELECT * from media"; 
+session_start();
+include_once "function.php";
+	$query = "SELECT * from media";
 	$result = mysql_query( $query );
 	if (!$result){
 	   die ("Could not query the media table in the database: <br />". mysql_error());
 	}
 ?>
-    
+
     <div style="background:#339900;color:#FFFFFF; width:150px;">Uploaded Media</div>
 	<table width="50%" cellpadding="0" cellspacing="0">
 		<?php
 			while ($result_row = mysql_fetch_row($result)) //filename, username, type, mediaid, path
-			{ 
+			{
 				$mediaid = $result_row[3];
 				$filename = $result_row[0];
 				$filenpath = $result_row[4];
 		?>
-        	 <tr valign="top">			
+        	 <tr valign="top">
 			<td>
-					<?php 
+					<?php
 						echo $mediaid;  //mediaid
 					?>
 			</td>
                         <td>
-            	            <a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $filename;?></a> 
+            	            <a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $filename;?></a>
                         </td>
                         <td>
             	            <a href="<?php echo $filenpath;?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a>
