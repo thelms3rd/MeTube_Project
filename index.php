fdcc9c67cf85b1023bd8a3bd8b8440b8907aa700
mpalme2 <mpalme2@g.clemson.edu>
2018-04-04 18:42:51 -0400

    index1

--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
- 
-<head>   
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS --> 
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 	<!-- Font_Awesome-->
 	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
-	
+
     <title>METUBE</title>
 </head>
 
-	
-	
+
+
 <!---------------------------------- BEGINNING OF THE BODY ------------------------------->
 <body>
-	
+
 <!--------------------------- Navigation Bar -------------------------------------------->
   <div id="Navigation_Bar">
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-		
-  
- 
+
+
+
   <!-- Links that are located on the Navagation Bar -->
-  
+
 	<!-- Left Nav Bar Elements -->
-		
+
   <ul class="navbar-nav navbar-left">
 	  <a class="navbar-brand" href="index.php">METUBE</a>
     <li class="nav-item">
@@ -43,11 +43,11 @@
       <a class="nav-link" href="#">AUDIO</a>
     </li>
    </ul>
-	    
-	
+
+
 	  <!-- Right Nav Bar Elements -->
 	<ul class="nav navbar-nav navbar-right">
-		
+
 		<!-- Search bar -->
 		<form class="navbar-form navbar-right" action="#" style="padding-right: 20px; width: 450px">
       <div class="input-group">
@@ -59,37 +59,73 @@
         </div>
       </div>
     </form>
-	  
+
 		<li class="nav-item">
 		  <a class="nav-link" href="register.php"> Register</a>
 	  </li>
-	  <li class="nav-item"> 
+	  <li class="nav-item">
 		  <a class="nav-link" href="login.php"> Login</a>
 		</li>
  </ul>
-	
+
 </div>
 </nav>
 </div>
 
 <!----------------------------MIDDLE SECTION ----------------------->
-	
+
 <div id="Middle" class="container-fluid">
 
 
 <!---  Display the Most Viewed Media --->
 <br>
-<h5> MOST VIEWED: </h5>	
+<h5> MOST VIEWED: </h5>
+<br/><br/>
+<?php
 
+	$query = "SELECT * from media";
+	$result = mysql_query( $query );
+	if (!$result){
+	   die ("Could not query the media table in the database: <br />". mysql_error());
+	}
+?>
+
+    <div style="background:#339900;color:#FFFFFF; width:150px;">Uploaded Media</div>
+	<table width="50%" cellpadding="0" cellspacing="0">
+		<?php
+			while ($result_row = mysql_fetch_row($result)) //filename, username, type, mediaid, path
+			{
+				$mediaid = $result_row[3];
+				$filename = $result_row[0];
+				$filenpath = $result_row[4];
+		?>
+        	 <tr valign="top">
+			<td>
+					<?php
+						echo $mediaid;  //mediaid
+					?>
+			</td>
+                        <td>
+            	            <a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $filename;?></a>
+                        </td>
+                        <td>
+            	            <a href="<?php echo $filenpath;?>" target="_blank"> <img src="<?php echo $filenpath;?>" alt="test" style="width:150:px"> <onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a>
+                        </td>
+		</tr>
+        	<?php
+			}
+		?>
+	</table>
+   </div>
 <hr>
 
 <!--- Display the New Uploads ---->
 <br>
 <h5>  NEW UPLOADS: </h5>
-	
+
 </div>
-	
-	
+
+
 <!---------------------------------------------------------------------->
 
 
@@ -102,4 +138,3 @@
 </body>
 
 </html>
-
