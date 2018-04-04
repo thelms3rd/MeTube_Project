bc36cf0b1a8e7957b7152a0e2ae3b00dbf9c2c64
Trey Helms <thelms3rd@247.137.21.198.guest.wifi.dyn.clemson.edu>
2018-04-04 14:43:03 -0400

    link on METUBE link

--- login.php
+++ login.php
@@ -29,7 +29,7 @@
 	<!-- Left Nav Bar Elements -->
 		
   <ul class="navbar-nav navbar-left">
-	  <a class="navbar-brand" href="#">METUBE</a>
+	  <a class="navbar-brand" href="index.php">METUBE</a>
     <li class="nav-item">
       <a class="nav-link" href="#">IMAGE</a>
     </li>
