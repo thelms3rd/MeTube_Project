4575b2a237f41adf45b6e818b91653283c9ca29e
Trey Helms <thelms3rd@36.137.21.198.guest.wifi.dyn.clemson.edu>
2018-04-10 08:18:47 -0400

    accountSettings Navbar

--- accountSettings.php
+++ accountSettings.php
@@ -75,25 +75,12 @@ include_once "function.php";
       </div>
     </form>
 
-<!---- if the user is not logged in display register and Login Links ---->
-<?php if(!(isset($_SESSION['loggedIn']))): ?>
 		<li class="nav-item">
-		  <a class="nav-link" href="register.php"> Register</a>
-	  </li>
-	<li class="nav-item"> 
-		  <a class="nav-link" href="login.php"> Login</a>
-		</li>
-
-<!----- else (the user is logged in) then display the account and register links ---->
-<?php else: ?>
-		<li class="nav-item">
-		  <a class="nav-link" href="accountSettings.php" name="accountSetting"> Account </a>
+		  <a class="nav-link" href="#" name="accountSetting"> Account </a>
 		</li>
 		<li class="nav-item">
 		  <a class="nav-link" href="logout.php" name="logout"> Logout</a>
 		</li>
-
-<?php endif; ?>
 			
  </ul>
 	
@@ -101,83 +88,9 @@ include_once "function.php";
 </nav>
 </div>
 <!------------- END OF NAVIGATION BAR ----------->
+
 	
-		
-<!----------------------------MIDDLE SECTION ----------------------->
-
-<div id="Middle" class="container-fluid">
-  <?php
-
-  	$query = "SELECT * from media";
-  	$result = mysql_query( $query );
-  	if (!$result){
-  	   die ("Could not query the media table in the database: <br />". mysql_error());
-  	}
-  ?>
-
-  <?php
-  	if(isset($_REQUEST['result']) && $_REQUEST['result']!=0)
-  	{
-  		echo upload_error($_REQUEST['result']);
-  	}
-	
-	if(isset($_POST['logout']))
-	{
-		session_unset();
-		session_destroy();
-		header('Location: https://webapp.cs.clemson.edu/~jlhelms/MeTube_Project/index.php');
-		echo $_SESSION['username'];
-	}
-  ?>
-
-
-
-<!---  Display the Most Viewed Media --->
-<br>
-<h5> MOST VIEWED: </h5>
-
-<?php
-  while ($result_row = mysql_fetch_row($result)) //filename, username, type, mediaid, path
-  {
-    $mediaid = $result_row[3];
-    $filename = $result_row[0];
-    $filenpath = $result_row[4];
-    $title = $result_row[5];
-    $date = $result_row[6];
-?>
-
-<!-- Display uploads -->
-
-<div class="row text-center">
-
-            <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
-                <div class = "panel panel-default">
-      			   <div class="img-thumbnail"> <a href="<?php echo $filenpath;?>"><img src="<?php echo $filenpath;?>" class="img-responsive" width = "400" height="200"><onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a></div>
-      			   <p> <?php echo $title ?> </p>
-                    <h6>views: </h6>
-                    <h6>upload date: <?php echo $date ?> </h6>
-                  <br>
-                  <br>
-    		  </div>
-            </div>
-</div>
-
-<?php
-}
-?>
-
-<hr>
-
-<!--- Display the New Uploads ---->
-<br>
-<h5>  NEW UPLOADS: </h5>
-
-</div>
-
-
 <!---------------------------------------------------------------------->
-
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
