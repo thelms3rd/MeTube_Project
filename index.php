1a3afc6a3718bfe5de5b90749fa61c905f201702
mpalme2 <mpalme2@g.clemson.edu>
2018-04-10 14:50:07 -0400

    description

--- index.php
+++ index.php
@@ -155,7 +155,7 @@ include_once "function.php";
     $filenpath = $result_row[4];
     $title = $result_row[5];
     $date = $result_row[6];
-
+    $description = $result_row[7];
 ?>
 
 <!-- Display uploads -->
--- media_upload.php
+++ media_upload.php
@@ -105,8 +105,8 @@ include_once "function.php";
    <input  name="file" type="file" size="50" />
 <h4 style="margin-bottom:0px; margin-top: 20px;">Title</h4><input maxlength="40" id="title" name="title" type="text" class="form-control" style="width: 550px;">
 <br>
-<!--<h4 style="margin-bottom:0px; margin-top: 20px;">Description (500 character max)</h5><input maxlength="500" id="description" name="description" type="text" class="form-control" rows="5" style="resize: none; width: 550px;"></textarea>
-<br>-->
+<h4 style="margin-bottom:0px; margin-top: 20px;">Description (500 character max)</h5><input maxlength="500" id="description" name="description" type="text" class="form-control" rows="5" style="resize: none; width: 550px;"></textarea>
+<br>
   <input value="Upload" name="submit" type="submit" />
   </p>
 
