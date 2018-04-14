ee37475d77550990f86449cf1deea0661e755899
Trey Helms <thelms3rd@196.4.23.172.generic.clemson.edu>
2018-04-14 17:40:02 -0400

    display image

--- image.php
+++ image.php
@@ -118,7 +118,7 @@ include_once "function.php";
 <!------------- END OF NAVIGATION BAR ----------->
 
 <?php
-session_start();
+//session_start();
 include_once "function.php";	
 
 ?>	
@@ -145,7 +145,10 @@ if (!$result){
 	$views = $row[10];
 	
 	?>
-	<div class="col">
+	<div class="row text-center">
+
+            <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+                <div class = "panel panel-default">
 						<div class = "panel panel-default">
 							<div class="img-thumbnail"> <a href="<?php echo $filenpath;?>"><img src="<?php echo $filenpath;?>" class="img-responsive" width = "400" height="200"><onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</onclick></a></div>
 							<p> <?php echo $title ?> </p>
@@ -156,6 +159,8 @@ if (!$result){
                     	<h6>category: <?php echo $category ?> </h6>
                   </div>
             	</div>
+		</div>
+	</div>
 	<?php
 }
 
