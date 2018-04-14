a59fdde6746edd05588aabf982dcbc0a5f9d83a3
Trey Helms <thelms3rd@128.143.21.198.guest.wifi.dyn.clemson.edu>
2018-04-14 13:11:14 -0400

    updated formatting

--- function.php
+++ function.php
@@ -359,7 +359,7 @@ function most_views()
 			?> 
 					<div class="col">
 						<div class = "panel panel-default">
-							<div class="img-thumbnail"> <a href="<?php echo $filenpath;?>"><img src="<?php echo $filenpath;?>" class="img-responsive" width = "400" height="200"><onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</onclick></a></div>
+							<div class="img-thumbnail"> <a href="<?php echo $filenpath;?>"><img src="<?php echo $filenpath;?>" class="img-responsive" width = "250" height="200"><onclick="javascript:saveDownload(<?php echo $result_row[4];?>);"><br>Download</onclick></a></div>
 							<p> <?php echo $title ?> </p>
                     	<h6>views: <?php echo $views ?></h6>
                     	<h6>upload date: <?php echo $date ?> </h6>
